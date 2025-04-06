7c107d16c50c8d808341504411599f05c300d21b
Pratyushsrivastavasgn <pratyushsrivastava1sgn@gmail.com>
2025-04-06 16:57:06 +0530

    Cn web  dev task 1

--- Pratyush cn/script.js
+++ Pratyush cn/script.js
@@ -7,11 +7,9 @@ const fields = ["name", "email", "phone", "summary", "education", "experience", 
       if (preview) preview.innerHTML = (value || "[Not Provided]").replace(/\n/g, "<br>");
     });
   }
-
   function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
-
   document.getElementById('resumeForm').addEventListener('input', updatePreview);
 
   document.getElementById('downloadBtn').addEventListener('click', () => {
@@ -19,10 +17,8 @@ const fields = ["name", "email", "phone", "summary", "education", "experience", 
 
     const element = document.getElementById('resumePreview');
 
-    // Add print mode styling for PDF
     element.classList.add('print-mode');
 
-    // Use timeout to allow style to apply
     setTimeout(() => {
       html2pdf().set({
         margin: [0.5, 0.5],
@@ -32,7 +28,7 @@ const fields = ["name", "email", "phone", "summary", "education", "experience", 
         jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
         pagebreak: { mode: ['css', 'legacy'] }
       }).from(element).save().then(() => {
-        // Revert back to screen styles
+   
         element.classList.remove('print-mode');
       });
     }, 100);
